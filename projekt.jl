e945ac39de76959d854f7b9a728a74bc7717567a
Aleksander Jakobczyk <255939@student.pwr.edu.pl>
2020-06-04 03:01:48 +0200

    Pierwsze kroki

--- projekt.jl
+++ projekt.jl
@@ -47,7 +47,23 @@ function get_radius(theta,e,semi_majo)
     return r
 end
 
+function transform_3D(x,y,Theta)
+    """
+    Funkcja obraca planety wpkół osi OY o podany kat.
 
+    x`=cosθ
+    y`=y
+    z=-sinθ
+    """
+
+    return x*cos(Theta),y,-x*sin(Theta)
+end
+
+
+
+
+#--------------------Testowe rysowane wykresu jednej plaentu-------------------
+#ustalanie danych planety
 period =AstroLib.planets["earth"].period
 e = AstroLib.planets["earth"].ecc
 semi_m =AstroLib.planets["earth"].axis
@@ -56,5 +72,33 @@ erth_E_list = get_eccentric_anomali(earth_M_list, e)
 erth_Theta_list = get_theta(earth_M_list, e)
 erth_R_list = get_radius(erth_Theta_list, e, semi_m)
 
-plot(erth_R_list.*cos.(erth_Theta_list),erth_R_list.*sin.(erth_Theta_list))
-plot!(aspect_ratio = :equal)
+##
+#Wyswietl wszystko
+# plot(erth_R_list.*cos.(erth_Theta_list),erth_R_list.*sin.(erth_Theta_list))
+# plot!(aspect_ratio = :equal)|>display
+
+
+
+maxr=maximum(erth_R_list)#W przyszłosci bedziemy sprawdzac maksymalna połoś
+#wspołzene
+xs_2D=cos.(erth_Theta_list)
+ys_2D=sin.(erth_Theta_list)
+xs_3D=[transform_3D.(xs_2D,ys_2D,pi/4)[i][1] for i in 1:length(xs_2D)]
+ys_3D=ys_2D
+zs_3D=[transform_3D.(xs_2D,ys_2D,pi/4)[i][3] for i in 1:length(xs_2D)]
+#animacja
+anim = @animate for i in 1:120###Bedzie trzeba zmienic
+    plot(aspect_ratio = :equal,xlim = (-maxr,maxr),
+        ylim = (-maxr,maxr), zlim = (-maxr,maxr),
+        foreground_color_legend = nothing,
+        background_color_legend = nothing, xlabel="x")
+
+    scatter!([0],[0],[0],markersize = 20,
+             markercolor = :yellow,
+             label="Sun")
+    scatter!([erth_R_list[i]*xs_3D[i]],
+             [erth_R_list[i]*ys_3D[i]],[erth_R_list[i]*zs_3D[i]],
+             label="Erth")
+
+end
+gif(anim, "anim_fps15.gif", fps = 15)
--- testy.jl
+++ testy.jl
@@ -1,40 +1,19 @@
-#Jesli to czytasz zanczy ze wsztyko działa
-
-#Plik do testeów
-
-using AstroLib,Measurements,Plots,Dates
-
-M = range(0, stop=2pi, length=1001)[1:end-1];
-plot()
-for ecc in (0, 0.5, 0.1)
-    plot!(cos.((trueanom.(kepler_solver.(M, ecc), ecc))), sin.((trueanom.(kepler_solver.(M, ecc), ecc))))
-end
-plot!()|>display
-plot(aspect_ratio = :equal)
-ecc=0.01671123
-a=1.49598261e+11
-M = range(0, stop=2pi, length=1001)[1:end-1];
-xs = (trueanom.(kepler_solver.(M, ecc), ecc))
-ys = (trueanom.(kepler_solver.(M, ecc), ecc))
-theta = mod2pi.(trueanom.(kepler_solver.(M, ecc), ecc))
-r=@. a*(1-ecc^2)/(1+ecc*cos(theta))
-
-plot(r.*cos.(theta),r.*sin.(theta),aspect_ratio=:equal)
-
-AstroLib.planets["mars"]
-(AstroLib.planets["earth"].period)/jdcnv(Day(1))
-
-daycnv(2459000)
-daycnv(2)
-daycnv(3)
-daycnv(4)4
-(jdcnv(1))
-Second(Day(1))
 
 
 
 M = LinRange(0, 2pi, 1001)[1:end-1];
 for ecc in (0.9)
     plot(M, mod2pi.(trueanom.(kepler_solver.(M, ecc), ecc)))
+end
+pusto=[]
+xs=LinRange(-2,2,100)
+nxs=cos(pi/2).* xs
+nzs=-sin(pi/2).*xs
+pusto=push!(pusto,xs.+1)
+plot(xs,xs,zeros(100))
+scatter!(nxs,xs,nzs,camera=(0,0))
+
+for i in 1:2
+    scatter!([1,2,3],lololo[i,:])
 end
 plot!()|>display
